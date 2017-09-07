7cefc8084f90462aa09780e797309e0aac55020b
Bowei Liu <liubowei19961227@gmail.com>
2017-09-07 19:31:20 +1000

    technical challenge

--- helloflask.py
+++ helloflask.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from werkzeug import secure_filename
+import os
 
 
 app = Flask(__name__,static_url_path = '/static')
 
-
+file_record = {}
 
 
 
@@ -18,28 +19,39 @@ def index():
 
 
 
-@app.route("/play_music",methods=['POST'])
+@app.route("/upload_file",methods=['POST'])
 def play_music():
-	return render_template('upload.html')
+	return render_template('upload2.html')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+	if request.method == 'POST':
+		f = request.files['file']
+		if f.filename in file_record:
+			return 'duplicated name found'
+		file_record[f.filename] = 1
+		
+
+
+		APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+		UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads')
+		app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+		filename = secure_filename(f.filename)
+		f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		
+		return 'file uploaded successfully'
 
 
 
 
-@app.route("/download_music",methods=['POST'])
+'''@app.route("/download_music",methods=['POST'])
 def dowload_music():
-	return "download music"
+	return "download music"'''
 
 
-@app.route("/read_input", methods=['POST'])
+'''@app.route("/read_input", methods=['POST'])
 def login():
-    return request.form['username']
+	return request.form['username']'''
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(host='0.0.0.0', port=9393)
